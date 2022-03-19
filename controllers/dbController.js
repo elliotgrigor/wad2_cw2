3763b12eb0ba046651f5cadceb39c73c06d37245
elliotgrigor <elliot.grigor@protonmail.com>
2022-03-19 14:32:08 +0000

    updated relative paths to db collections

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -1,21 +1,21 @@
 const Datastore = require('@seald-io/nedb');
 
 exports.menu = new Datastore({
-  filename: '../db/menu.collection',
+  filename: './db/menu.collection',
   autoload: true,
 });
 
 exports.staff = new Datastore({
-  filename: '../db/staff.collection',
+  filename: './db/staff.collection',
   autoload: true,
 });
 
 exports.messages = new Datastore({
-  filename: '../db/messages.collection',
+  filename: './db/messages.collection',
   autoload: true,
 });
 
 exports.faqs = new Datastore({
-  filename: '../db/faqs.collection',
+  filename: './db/faqs.collection',
   autoload: true,
 });
