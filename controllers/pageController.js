171d554cff9c200b78b59ee4c2d837a215abb764
elliotgrigor <elliot.grigor@protonmail.com>
2022-03-19 15:52:57 +0000

    added routing to staff login page

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -3,3 +3,7 @@ const { render } = require('mustache-express');
 exports.home = (req, res) => {
   res.render('home', {});
 };
+
+exports.login = (req, res) => {
+  res.render('login', {});
+};
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const pages = require('../controllers/pageController');
 
 router.get('/', pages.home);
+router.get('/login', pages.login);
 
 module.exports = router;
