8aeba7d4dccbc3252c87af43d5d1155cb57e9365
elliotgrigor <elliot.grigor@protonmail.com>
2022-03-19 14:28:05 +0000

    updated db collection exports

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -1,26 +1,21 @@
 const Datastore = require('@seald-io/nedb');
 
-const menu = new Datastore({
+exports.menu = new Datastore({
   filename: '../db/menu.collection',
   autoload: true,
 });
 
-const staff = new Datastore({
+exports.staff = new Datastore({
   filename: '../db/staff.collection',
   autoload: true,
 });
 
-const messages = new Datastore({
+exports.messages = new Datastore({
   filename: '../db/messages.collection',
   autoload: true,
 });
 
-const faqs = new Datastore({
+exports.faqs = new Datastore({
   filename: '../db/faqs.collection',
   autoload: true,
 });
-
-exports.menu     = menu;
-exports.staff    = staff;
-exports.messages = messages;
-exports.faqs     = faqs;
