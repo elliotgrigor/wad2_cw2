7601eaee65470e4f580813aac9824fb8389b89e8
elliotgrigor <elliot.grigor@protonmail.com>
2022-03-20 22:46:02 +0000

    moved passport authentication to separate module

---
+++ controllers/authController.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+const StaffModel = require('../models/StaffModel');
+const staff = new StaffModel(); // *** USING IN-MEMORY DB ***
+
+passport.use(new LocalStrategy((username, password, done) => {
+  staff.db.findOne(
+    { staffId: username },
+    (err, doc) => {
+      if (err) return done(err);
+      if (!doc) return done(null, false, { message: 'Incorrect username or password.' });
+
+      bcrypt.compare(password, doc.password, (err, result) => {
+        if (err) return done(err);
+        if (!result) {
+          return done(null, false, { message: 'Incorrect username or password.' });
+        }
+        return done(null, doc);
+      });
+    }
+  );
+}));
+
+passport.serializeUser((user, done) => {
+  process.nextTick(() => {
+    done(null, { ...user });
+  });
+});
+
+passport.deserializeUser((user, done) => {
+  process.nextTick(() => {
+    return done(null, user);
+  });
+});
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,41 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
 
-const StaffModel = require('../models/StaffModel');
-const staff = new StaffModel();
-
-passport.use(new LocalStrategy((username, password, done) => {
-  staff.db.findOne(
-    { staffId: username },
-    (err, doc) => {
-      if (err) return done(err);
-      if (!doc) return done(null, false, { message: 'Incorrect username or password.' });
-
-      bcrypt.compare(password, doc.password, (err, result) => {
-        if (err) return done(err);
-        if (!result) {
-          return done(null, false, { message: 'Incorrect username or password.' });
-        }
-        return done(null, doc);
-      });
-    }
-  );
-}));
-
-passport.serializeUser((user, done) => {
-  process.nextTick(() => {
-    done(null, { ...user });
-  });
-});
-
-passport.deserializeUser((user, done) => {
-  process.nextTick(() => {
-    return done(null, user);
-  });
-});
+require('../controllers/authController');
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/staff',
