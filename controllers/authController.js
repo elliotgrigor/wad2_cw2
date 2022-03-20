c1b612e02c3ec721ccfa363fcf9a2c4f21cc18ef
elliotgrigor <elliot.grigor@protonmail.com>
2022-03-20 23:05:02 +0000

    added logout functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,3 +34,11 @@ passport.deserializeUser((user, done) => {
     return done(null, user);
   });
 });
+
+exports.logout = (req, res) => {
+  req.session.destroy(err => {
+    if (err) return console.log(err);
+    res.clearCookie('connect.sid');
+    res.redirect('/');
+  });
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-require('../controllers/authController');
+const auth = require('../controllers/authController');
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/staff',
   failureRedirect: '/login',
 }));
+router.get('/logout', auth.logout);
 
 module.exports = router;
