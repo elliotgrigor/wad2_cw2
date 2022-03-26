f90457dbd79eea0e8ad37020e9a54d4cf12dabd4
elliotgrigor <elliot.grigor@protonmail.com>
2022-03-26 01:18:15 +0000

    put database paths into env variables

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -1,21 +1,21 @@
 const Datastore = require('@seald-io/nedb');
 
 exports.menu = new Datastore({
-  filename: './db/menu.collection',
+  filename: process.env.MENU_DB,
   autoload: true,
 });
 
 exports.staff = new Datastore({
-  filename: './db/staff.collection',
+  filename: process.env.STAFF_DB,
   autoload: true,
 });
 
 exports.messages = new Datastore({
-  filename: './db/messages.collection',
+  filename: process.env.MSG_DB,
   autoload: true,
 });
 
 exports.faqs = new Datastore({
-  filename: './db/faqs.collection',
+  filename: process.env.FAQ_DB,
   autoload: true,
 });
