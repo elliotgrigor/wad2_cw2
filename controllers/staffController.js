f52e6909c04bbf7e974febce4d7a9a025857c68c
elliotgrigor <elliot.grigor@protonmail.com>
2022-04-12 15:55:29 +0100

    added dish add view & inserting dish to database functionality

--- controllers/staffController.js
+++ controllers/staffController.js
@@ -1,4 +1,48 @@
+const Menu = require('../models/MenuModel');
 
 exports.dashboard = (req, res) => {
   res.render('staff/dashboard', {});
+};
+
+exports.dishes = (req, res) => {
+  res.render('staff/menus', {});
+};
+
+exports.addDish = (req, res) => {
+  const css = [
+    { url: '/css/staff/addDish.css' },
+  ];
+
+  res.render('staff/addDish', { css });
+};
+
+exports.addDishPOST = (req, res) => {
+  const {
+    name, desc, dish_type, price, is_special,
+    ingredients, allergens, allergy_advice,
+  } = req.body;
+
+  const ingredientList = ingredients.replace(/\s/g, '').split(',');
+  const allergenList = allergens.replace(/\s/g, '').split(',');
+
+  Menu
+    .insert({
+      name,
+      description: desc,
+      content: {
+        ingredients: ingredientList,
+        allergyInfo: {
+          allergens: allergenList,
+          advice: allergy_advice,
+        },
+      },
+      chefSpecial: is_special === 'on' ? true : false,
+      dishType: dish_type,
+      price,
+    })
+    .then(doc => {
+      console.log('Inserted:', doc);
+      res.redirect('/staff'); 
+    })
+    .catch(err => console.log(err));
 };
--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const staff = require('../controllers/staffController');
 
 router.get('/dashboard', staff.dashboard);
+router.get('/dishes', staff.dishes);
+router.get('/dishes/add', staff.addDish);
+router.post('/dishes/add', staff.addDishPOST);
+
 router.get('*', (_, res) => res.redirect('/staff/dashboard'));
 
 module.exports = router;
