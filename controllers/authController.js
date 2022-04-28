430a03671d127ad17e0ad554e5b18fb4c9741030
elliotgrigor <elliot.grigor@protonmail.com>
2022-04-28 21:52:48 +0100

    changed login handler to async function

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,21 +3,24 @@ const bcrypt = require('bcrypt');
 
 const Staff = require('../models/StaffModel');
 
-exports.login = (req, res) => {
-  Staff.findOne({ staffId: req.body.id }, { 'staffId': 1, 'password': 1 })
-    .then(user => {
-      if (!user) return res.redirect('/login');
+exports.login = async (req, res) => {
+  try {
+    const user = await Staff.findOne({ staffId: req.body.id }, { 'staffId': 1, 'password': 1 })
 
-      bcrypt.compare(req.body.password, user.password, (err, result) => {
-        if (err || !result) return res.redirect('/login');
+    if (!user) return res.redirect('/login');
 
-        const token = jwt.sign({ id: user.staffId }, process.env.SECRET, {
-          expiresIn: '1800s', // 30 minutes
-        });
-        res.cookie('jwt', token).redirect('/staff');
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+      if (err || !result) return res.redirect('/login');
+
+      const token = jwt.sign({ id: user.staffId }, process.env.SECRET, {
+        expiresIn: '1800s', // 30 minutes
       });
-    })
-    .catch(err => console.log(err));
+      res.cookie('jwt', token).redirect('/staff');
+    });
+  }
+  catch (err) {
+    console.log(err);
+  }
 };
 
 exports.logout = (req, res) => {
