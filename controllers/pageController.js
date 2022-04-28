3f13ea6827ce0a0991f450d7f5fb045d581258ee
elliotgrigor <elliot.grigor@protonmail.com>
2022-04-29 00:09:03 +0100

    added favicon & dynamic tab titles

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,19 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 exports.home = (req, res) => {
-  res.render('home', {});
+  res.render('home', { pageTitle: 'Home' });
 };
 
 exports.menu = (req, res) => {
-  res.render('menu', {});
+  res.render('menu', { pageTitle: 'Menu' });
 };
 
 exports.contact = (req, res) => {
-  res.render('contact', {});
+  res.render('contact', { pageTitle: 'Contact' });
 };
 
 exports.aboutUs = (req, res) => {
-  res.render('about', {});
+  res.render('about', { pageTitle: 'About us' });
 };
 
 exports.login = (req, res) => {
@@ -23,12 +23,12 @@ exports.login = (req, res) => {
   ];
 
   if (!accessToken) {
-    return res.render('login', { css });
+    return res.render('login', { pageTitle: 'Login', css });
   }
 
   jwt.verify(accessToken, process.env.SECRET, (err, _) => {
     if (err) {
-      return res.render('login', { css });
+      return res.render('login', { pageTitle: 'Login', css });
     }
     return res.redirect('/staff/dashboard');
   });
