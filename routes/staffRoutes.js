b06a103caf60608ae9a263e15f6806ec13ed8151
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-04 00:51:54 +0100

    reformatted route declarations

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -3,21 +3,28 @@ const router = express.Router();
 
 const staff = require('../controllers/staffController');
 
+// GET
 router.get('/dashboard', staff.dashboard);
+
 router.get('/dishes', staff.dishes);
 router.get('/dishes/add', staff.addDish);
 router.get('/dishes/:id', staff.editDish);
+
 router.get('/faqs', staff.faqs);
 router.get('/faqs/add', staff.addFAQ);
 router.get('/faqs/:id', staff.editFAQ);
+
 router.get('/register', staff.registerUser);
 
+// POST
 router.post('/dishes/add', staff.addDishPOST);
 router.post('/dishes/:id/update', staff.editDishPOST);
 router.post('/dishes/:id/delete', staff.deleteDish);
+
 router.post('/faqs/add', staff.addFAQPOST);
 router.post('/faqs/:id/update', staff.editFAQPOST);
 router.post('/faqs/:id/delete', staff.deleteFAQ);
+
 router.post('/register', staff.registerUserPOST);
 
 router.get('*', (_, res) => res.redirect('/staff/dashboard'));
