e36d137b2474fea700a2456478b0860fbc932d59
elliotgrigor <elliot.grigor@protonmail.com>
2022-05-03 18:16:32 +0100

    updated route name & added about page stylesheet

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -8,7 +8,7 @@ router.get('/menu', pages.menu);
 router.get('/menu/:slug', pages.viewDish);
 router.get('/contact', pages.contact);
 router.get('/faqs', pages.faqs);
-router.get('/aboutus', pages.aboutUs);
+router.get('/about-us', pages.aboutUs);
 
 router.get('/login', pages.login);
 
